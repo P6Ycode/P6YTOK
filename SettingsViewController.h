e5a2c7116a193d0501f7f6bcc47f33f941a1b8c0
P6Ycode <thalightskintiller@gmail.com>
2026-07-23 06:45:30 -0400

    Replace legacy settings controller interface

--- SettingsViewController.h
+++ SettingsViewController.h
@@ -1,11 +1,4 @@
 #import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#import <CepheiPrefs/CepheiPrefs.h>
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
 
-@interface SettingsViewController : HBListController
-- (instancetype)init;
-- (id)readPreferenceValue:(PSSpecifier *)specifier;
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
+@interface SettingsViewController : UITableViewController
 @end
