5e2043be20c21342641addecdb33ba430c0eeced
P6Ycode <thalightskintiller@gmail.com>
2026-07-23 11:49:10 -0400

    Add full-quality media resolver interface

---
+++ P6YMediaQuality.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+NSArray *P6YSortURLItems(NSArray *items);
+NSURL * _Nullable P6YBestURLFromModel(id _Nullable urlModel);
+NSURL * _Nullable P6YHQVideoURL(id _Nullable model);
+NSArray<NSURL *> *P6YHQPhotoURLs(id _Nullable model);
+NS_ASSUME_NONNULL_END
