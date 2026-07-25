73834b6fa9b049d8dfac4ab6371dc31ffa62dba5
P6Ycode <thalightskintiller@gmail.com>
2026-07-25 03:02:11 -0400

    Add login-safe compat core header

---
+++ P6YCompat/P6YCompatCore.h
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, P6YCompatFeatureGroup) {
+    P6YCompatFeatureGroupSettings = 0,
+    P6YCompatFeatureGroupDownloads,
+    P6YCompatFeatureGroupFeedUI,
+    P6YCompatFeatureGroupProfile,
+    P6YCompatFeatureGroupAdFiltering,
+    P6YCompatFeatureGroupBrowserRedirects,
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString * const P6YCompatDidEnableFeatureGroupsNotification;
+extern NSString * const P6YCompatLoginSafeModeChangedNotification;
+
+void P6YCompatBootstrap(void);
+void P6YCompatObserveViewController(UIViewController *viewController);
+void P6YCompatEnableFeatureGroupsIfReady(void);
+
+BOOL P6YCompatIsEnabled(void);
+BOOL P6YCompatIsLoginSafeModeEnabled(void);
+BOOL P6YCompatAreDelayedFeatureGroupsEnabled(void);
+BOOL P6YCompatShouldRunFeatureGroup(P6YCompatFeatureGroup group);
+
+NSString *P6YCompatTikTokVersion(void);
+BOOL P6YCompatLooksLikeInjectedIPA(void);
+BOOL P6YCompatClassExists(NSString *className);
+BOOL P6YCompatInstanceSelectorExists(NSString *className, SEL selector);
+BOOL P6YCompatClassSelectorExists(NSString *className, SEL selector);
+
+NSString *P6YCompatFeatureGroupName(P6YCompatFeatureGroup group);
+void P6YCompatLog(NSString *format, ...) NS_FORMAT_FUNCTION(1, 2);
+
+NS_ASSUME_NONNULL_END
