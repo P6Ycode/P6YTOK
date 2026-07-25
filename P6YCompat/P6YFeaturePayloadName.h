8f6ba06cfae6896e542ef3c6a9eaa8ee18c9b6d6
P6Ycode <thalightskintiller@gmail.com>
2026-07-25 03:26:45 -0400

    Add delayed payload name header

---
+++ P6YCompat/P6YFeaturePayloadName.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+// This file is overwritten by scripts/wire_p6ycompat.sh during IPA builds.
+// The fallback keeps local source parsing and editor tooling happy.
+#ifndef P6Y_FEATURE_DYLIB_NAME
+#define P6Y_FEATURE_DYLIB_NAME @"P6YFeaturePayload.dylib"
+#endif
